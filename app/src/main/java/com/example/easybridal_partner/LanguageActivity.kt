6e8c9e69510dc286f4f0ec775dfe736bebb9da62
agirahman <agi.rahman.s@gmail.com>
2024-06-05 23:14:42 +0700

    update change Language menu (nambahin animasi)

--- app/src/main/java/com/example/easybridal_partner/LanguageActivity.kt
+++ app/src/main/java/com/example/easybridal_partner/LanguageActivity.kt
@@ -1,24 +1,71 @@
 package com.example.easybridal_partner
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class LanguageActivity : AppCompatActivity() {
 
-
-
-
-    private fun initComponents(){
-
-    }
+    private var selectedButton: LinearLayout? = null
+    private var selectedArrow: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_language)
 
-        initComponents()
+        val englishButton: LinearLayout = findViewById(R.id.english_language)
+        val arrowEnglish: ImageView = findViewById(R.id.arrow_english)
 
+        val indonesiaButton: LinearLayout = findViewById(R.id.indonesia_language)
+        val arrowIndonesia: ImageView = findViewById(R.id.arrow_indonesia)
 
+        val usaButton: LinearLayout = findViewById(R.id.usa_language)
+        val arrowUsa: ImageView = findViewById(R.id.arrow_usa)
+
+        setLanguageSelection(englishButton, arrowEnglish)
+        setLanguageSelection(indonesiaButton, arrowIndonesia)
+        setLanguageSelection(usaButton, arrowUsa)
+    }
+
+    private fun setLanguageSelection(button: LinearLayout, arrow: ImageView) {
+        button.setOnClickListener {
+            if (button != selectedButton) {
+                resetSelectedLanguage()
+                animateButton(button)
+                button.background = getSelectedOutlineDrawable()
+                arrow.visibility = View.VISIBLE
+                selectedButton = button
+                selectedArrow = arrow
+            } else {
+                resetSelectedLanguage()
+            }
+        }
+    }
+
+    private fun resetSelectedLanguage() {
+        selectedButton?.background = getNormalOutlineDrawable()
+        selectedArrow?.visibility = View.GONE
+        selectedButton = null
+        selectedArrow = null
+    }
+
+    private fun animateButton(button: LinearLayout) {
+        val fadeIn = AlphaAnimation(0f, 1f)
+        fadeIn.duration = 300
+        button.startAnimation(fadeIn)
+    }
+
+    private fun getNormalOutlineDrawable(): Drawable? {
+        return ContextCompat.getDrawable(this, R.drawable.outline_grey)
+    }
+
+    private fun getSelectedOutlineDrawable(): Drawable? {
+        return ContextCompat.getDrawable(this, R.drawable.searchview_outline)
     }
 }
--- app/src/main/java/com/example/easybridal_partner/WithdrawSuccessMenu.kt
+++ app/src/main/java/com/example/easybridal_partner/WithdrawSuccessMenu.kt
@@ -1,12 +1,28 @@
 package com.example.easybridal_partner
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class WithdrawSuccessMenu : AppCompatActivity() {
 
+    private lateinit var done: Button
+
+    private fun initComponents() {
+        done = findViewById(R.id.doneButton)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_withdraw_success)
+
+        initComponents()
+
+        done.setOnClickListener {
+            val intent = Intent(this@WithdrawSuccessMenu, PaymentActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
